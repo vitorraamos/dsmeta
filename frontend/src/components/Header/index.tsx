07dba76028c6f1fb77dec38f9dd9cc7bce6d89be
Vitor Ramos <vitor.raamos@gmail.com>
2022-07-12 11:20:38 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
-import logo from '../../assets/img/logo.svg'
+import logo from '../../assets/img/logo.svg';
 
-import './styles.css'
+import './styles.css';
 
 function Header() {
     return (
@@ -14,4 +14,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
